78b661b1b8b5731a489c0f3eb821ca681d577ad9
Katie Purdy <katiepurdy131@gmail.com>
2019-11-19 19:39:34 -0400

    Update for heroku

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(`${process.env.MONGO_URI}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
