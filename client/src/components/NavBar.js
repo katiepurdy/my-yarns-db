0b40162cbf3e52082e44dab9f1248517b89e5e41
Katie Purdy <katiepurdy131@gmail.com>
2019-11-25 21:15:34 -0400

    Update app name in NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -22,7 +22,7 @@ const NavBar = () => {
             <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path>
             <circle cx="12" cy="13" r="4"></circle>
           </svg>
-          <strong>My Fullstack App</strong>
+          <strong>My Yarns DB</strong>
         </a>
         <button
           className="navbar-toggler"
