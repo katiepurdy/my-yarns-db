cc4b5579e9be71f01546be95feff209c14a967d9
Katie Purdy <katiepurdy131@gmail.com>
2019-11-25 21:10:53 -0400

    Add heroku-postbuild script and change static files in express

--- app.js
+++ app.js
@@ -47,6 +47,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
+}
 app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
